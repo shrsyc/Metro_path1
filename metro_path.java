6414b429af0da62bee9542b1a76e0618d68c9a4c
shrsyc <shrsyc@gmail.com>
2022-11-26 00:15:46 +0530

    first commit

---
+++ metro_path.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class metro_path {
+    public static void main(String[] args) {
+        System.out.println("Enter the desired option");
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        switch (a) {
+            case 1:System.out.println("option 1");
+                break;
+            case 2:System.out.println("option 2");
+                 break;    
+        
+            default:
+                break;
+        }
+        
+    }
+    
+}
